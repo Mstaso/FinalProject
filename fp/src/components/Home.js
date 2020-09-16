5d0dd67c6efd3765a0e1a019cc5c81e389fa435b
mstaso <mstaso1@gmail.com>
2020-09-16 17:19:43 -0400

    added more images to homepage

--- fp/src/components/Home.js
+++ fp/src/components/Home.js
@@ -4,6 +4,8 @@ import BusinessContainer from '../containers/BusinessContainer'
 
 const banner = require('../banner.jpg')
 
+const homebanner1 = require('../homebanner1.png')
+
 class Home extends React.Component {
 
 
@@ -12,16 +14,29 @@ class Home extends React.Component {
             <div >
                <div className="banner-div">
                <img id="banner" src={banner}/>
+               <div className="banner-info">
+                <h3>Match With Your Next Employer</h3> 
+                <br></br>
+                <p>Find and land job openings by taking courses that businesses find integral</p>
+                <button>Sign Up</button>
+               </div>
                 </div> 
+                <br></br>
+                <br></br>
+                <br></br>
             <div class="divider">
-            <h2>Featured Businesses</h2>
+            <h3>Featured Businesses</h3>
+            <br></br>
+            <br></br>
             </div>
             <br></br>
             <br></br>
             <div id="columns" > 
                 <BusinessContainer />
             </div>
-            
+            <div class="home-courses">
+            </div>
+            <img class="homebanner1" src={homebanner1}></img>
             <br></br>
             <br></br>  
             <div class="divider">
