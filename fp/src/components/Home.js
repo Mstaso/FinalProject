0e1ea3205cca8dab8cdbbdf99d2d50bf435156db
mstaso <mstaso1@gmail.com>
2020-09-17 12:33:49 -0400

    more changes to home page

--- fp/src/components/Home.js
+++ fp/src/components/Home.js
@@ -6,6 +6,8 @@ const banner = require('../banner.jpg')
 
 const homebanner1 = require('../homebanner1.png')
 
+const homebanner2 = require('../homebanner2.jpg')
+
 class Home extends React.Component {
 
 
@@ -44,7 +46,7 @@ class Home extends React.Component {
                 <BusinessContainer />
             </div>
             <div class="home-courses">
-            <h3>Computer Science</h3> 
+            <h3>Computer Science Courses</h3> 
             <br></br>
             <p>Those studying computer science may end up taking a similarly broad range of career pathways. Many CS majors focus on building skills in software programming, coding, and web development, all of which are highly sought after by a wide variety of companies. However, others may focus on more specialized topics such as algorithmic problem solving, machine learning and artificial intelligence, cybersecurity, and even robotics!</p>
             <br></br>
@@ -53,14 +55,40 @@ class Home extends React.Component {
             <img class="homebanner1" src={homebanner1}></img>
             <br></br>
             <br></br>  
+         
+            <div class="home-courses-2">
+
+            <h3>Business Courses</h3> 
+            <br></br>
+            <p>If you’re looking to advance your career in business, there are plenty of online courses available that can help you build these skills right away. Courses in finance, accounting, and business modeling can give you the tools you need to manage your cashflow wisely. Business planning, value chain management, and project management can help you optimize your operations for success.  Whatever your business background, online courses can take you further!</p>
+            <br></br>
+            <button>View Courses</button>
+            <img src={homebanner2}></img>
+
+            </div>
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>  
+            <br></br>  
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>  
+            <br></br> 
             <div class="divider">
-            <h2>Featured Courses</h2>
+            <h3>Featured Courses</h3>
             </div>   
             <br></br>
             <br></br>
             <br></br>
             <br></br>  
             <br></br>  
+            <br></br>
+            <br></br>
+            <br></br>
+            <br></br>  
+            <br></br> 
             <div id="columnshome">   
                 <CourseContainer />
             </div>    
