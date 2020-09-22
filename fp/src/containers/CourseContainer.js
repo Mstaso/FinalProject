b90d27afb7dd0aa447f9c0ab804657330fe7d0d8
mstaso <mstaso1@gmail.com>
2020-09-21 21:46:21 -0400

    added some pagination but other functionalities not working now

--- fp/src/containers/CourseContainer.js
+++ fp/src/containers/CourseContainer.js
@@ -4,36 +4,90 @@ import {Route, Switch} from 'react-router-dom'
 import { connect } from 'react-redux'
 import { getCourses } from '../redux/actions'
 import FilterSetting from '../components/FilterSetting'
+import ReactPaginate from 'react-paginate'
 
 
 class CourseContainer extends React.Component {
 
     state = {
-        category: ''
-    }
-    componentDidMount(){
-        this.props.fetchCourses()
+        category: '',
+        offset: 0,
+        courses: [],
+        perPage: 10,
+        currentPage: 0,
+        pageCount: 100
     }
 
-    commentCreater = (content, foundcourse_id) => {
-        fetch('http://localhost:3000/api/v1/comments',{
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-                 'accepts': 'application/json',
-              },
-            body: JSON.stringify({ comment: {
-                content: content,
-                user_id: this.props.loggedInUser.id,
-                course_id: foundcourse_id
-            }})
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-            this.props.fetchCourses()
-        })
+    componentDidMount(){
+        this.props.fetchCourses()
+        this.receivedCoursestwo() 
     }
+
+    // receivedCourses() {
+    //     fetch("http://localhost:3000/api/v1/courses")
+    //     .then(resp => resp.json())
+    //     .then(data => {
+    //         const slice = data.slice(this.state.offset, this.state.offset + this.state.perPage)
+    //         const postData = slice.map(course => 
+                
+    //             <Course course={course} key={course.id}/>)
+    //             this.setState({
+    //                 pageCount: Math.ceil(this.props.courses.length / this.state.perPage),
+    //                 courses: postData
+    //             })
+
+    //     })
+         
+    // }
+    receivedCoursestwo() {
+       
+            const slice = this.props.courses.slice(this.state.offset, this.state.offset + this.state.perPage)
+            const postData = slice.map(course => 
+                
+                <Course course={course} key={course.id}/>)
+                    
+                    this.setState({
+                    pageCount: Math.ceil(1000 / this.state.perPage),
+                    courses: postData
+                })
+        
+    }
+
+
+    handlePageClick = (e) => {
+        const selectedPage = e.selected;
+        const offset = selectedPage * this.state.perPage;
+
+        this.setState({
+            currentPage: selectedPage,
+            offset: offset
+        }, () => {
+            this.receivedCoursestwo()
+        });
+
+    };
+
+
+
+    // commentCreater = (content, foundcourse_id) => {
+    //     fetch('http://localhost:3000/api/v1/comments',{
+    //         method: 'POST',
+    //         headers: {
+    //             'Content-Type': 'application/json',
+    //              'accepts': 'application/json',
+    //           },
+    //         body: JSON.stringify({ comment: {
+    //             content: content,
+    //             user_id: this.props.loggedInUser.id,
+    //             course_id: foundcourse_id
+    //         }})
+    //     })
+    //     .then(response => response.json())
+    //     .then(data => {
+    //         console.log(data)
+    //         this.props.fetchCourses()
+    //     })
+    // }
 
     returnCourses = (newcategory) => {
         console.log(newcategory)
@@ -54,29 +108,30 @@ class CourseContainer extends React.Component {
     
     render(){
         // let courses = this.props.courses.map(course => <Course key={course.id} course={course}/>)
-        let coursesToDisplay = []
-        let courses = this.props.courses.filter(course => 
-            course.name.toLowerCase().includes(this.props.searchValue.toLowerCase()))
-        console.log(this.state.category)
-        this.state.category == '' ? 
-        coursesToDisplay = courses.map(course => 
-            <Course key={course.id} course={course}/>)   
-            :
-        coursesToDisplay = courses.filter(course =>
-            course.category === this.state.category).map(course => 
-            <Course key={course.id} course={course}/>)      
+        // let coursesToDisplay = []
+        // let courses = this.props.courses.filter(course => 
+        //     course.name.toLowerCase().includes(this.props.searchValue.toLowerCase()))
+        // this.state.category == '' ? 
+        // coursesToDisplay = courses.map(course => 
+        //     <Course key={course.id} course={course}/>)   
+        //     :
+        // coursesToDisplay = courses.filter(course =>
+        //     course.category === this.state.category).map(course => 
+        //     <Course key={course.id} course={course}/>)      
 
-        // .map(course => <Course key={course.id} course={course}/>)
-        let homeCourses = this.props.courses.splice(0,12).map(course => <Course key={course.id} course={course}/>)
+        // // .map(course => <Course key={course.id} course={course}/>)
+        // let homeCourses = this.props.courses.splice(0,12).map(course => <Course key={course.id} course={course}/>)
         return (
             <>
-            {this.props.courses.length === 0 ? <h1>Loading</h1>: 
+            {/* {this.props.courses.length === 0 ? <h1>Loading</h1>:  */}
             
             <>
             <Switch>
                 <Route path='/courses/:id' render={({ match }) => {
                     let id = parseInt(match.params.id)
+                    console.log(id, this.props.courses)
                     let foundCourse = this.props.courses.find((course)=> course.id === id)
+                    console.log(foundCourse)
                     return <Course commentCreater={this.commentCreater} foundCourse={foundCourse} userAdder={this.userAdder} />
                 }}/>
                 <Route path="/courses" render={() => {
@@ -95,12 +150,23 @@ class CourseContainer extends React.Component {
                         <br></br>
                         <br></br>
                             {
-                                this.props.courses.length === 0 ? <h1>Loading</h1> :
+                                // this.props.courses.length === 0 ? <h1>Loading</h1> :
                                 <div id="columnscourses">
-                                {coursesToDisplay}
+                                {this.state.courses}
                                 </div>
                             }
-                        
+                <ReactPaginate
+                    previousLabel={"prev"}
+                    nextLabel={"next"}
+                    breakLabel={"..."}
+                    breakClassName={"break-me"}
+                    pageCount={this.state.postData}
+                    marginPagesDisplayed={2}
+                    pageRangeDisplayed={5}
+                    onPageChange={this.handlePageClick}
+                    containerClassName={"pagination"}
+                    subContainerClassName={"pages pagination"}
+                    activeClassName={"active"}/>
                         
                         </>
                     )
@@ -112,7 +178,7 @@ class CourseContainer extends React.Component {
                             {
                                 this.props.courses.length === 0 ? <h1>Loading</h1> :
                                 <div id="columnscourses">
-                                {homeCourses}
+                                {this.state.courses}
                                 </div>
                             }
                         
@@ -125,7 +191,7 @@ class CourseContainer extends React.Component {
             </>
             
             
-            }
+            
             </>
         )
     }
